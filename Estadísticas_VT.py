1d0ba1878e00b2104a5f56c0d2d710e96c8a15fb
StablumA <129173504+StablumA@users.noreply.github.com>
2023-04-05 18:53:07 -0300

    Agregado mapa de calor

--- Estadísticas_VT.py
+++ Estadísticas_VT.py
@@ -4,15 +4,18 @@ import seaborn as sn
 import geopandas as gpd
 import folium
 from folium import plugins
-df = pd.read_csv("10-TX-SData.csv",sep=',',parse_dates=['Fecha Hora'],dayfirst=True)
-df['Fecha Hora']=pd.to_datetime(df['Fecha Hora'])
+from folium.map import Layer
+from jinja2 import Template
+df = pd.read_csv("10-TX-SData.csv",sep=',',parse_dates=['FECHATRX'],dayfirst=True)
+df['FECHATRX']=pd.to_datetime(df['FECHATRX'])
 
 # elimino primer columna
 df=df.drop(df.columns[[0]],axis=1)
 
+df['Hora']=df['FECHATRX'].dt.hour
 # filtro por tipo de día
-df['Dia']=df['Fecha Hora'].dt.day
-df['nDia']=df['Fecha Hora'].dt.day_name()
+df['Dia']=df['FECHATRX'].dt.day
+df['nDia']=df['FECHATRX'].dt.day_name()
 #saco los sabados y domingos
 dfhabil= df[df['nDia'].isin(['Monday','Tuesday','Wednesday','Thursday','Friday'])]
 # saco los feriados
@@ -21,12 +24,12 @@ dfhabil= dfhabil[dfhabil['Dia']!= (i for i in feriados)]
 
 print(dfhabil)
 ## Veo num. de transacciones por  'Linea' en el total de días habiles
-PromedioHabiles= dfhabil.groupby(['Ramal']).size().to_frame('NTrans').reset_index()
+PromedioHabiles= dfhabil.groupby(['Ramal']).size().to_frame('NumerodeTrans').reset_index()
 Thabiles= len(dfhabil['Dia'].unique())
 # obtengo el promedio por día x linea
 
-PromedioHabiles['NTransPromedio']= PromedioHabiles['NTrans'].apply(lambda NTrans: (NTrans/Thabiles))
-#PromedioHabiles= PromedioHabiles.astype({"NTrans":"int"})
+PromedioHabiles['NTransPromedio']= PromedioHabiles['NumerodeTrans'].apply(lambda NTrans: (NTrans/Thabiles))
+#PromedioHabiles= PromedioHabiles.astype({"NumerodeTrans:"int"})
 print(PromedioHabiles)
 
 total = PromedioHabiles["NTransPromedio"].sum()
@@ -34,22 +37,139 @@ factor = 100/total
 
 PromedioHabiles['FracUso']= PromedioHabiles["NTransPromedio"].apply(lambda cant: (cant*factor))
 PromedioHabiles= PromedioHabiles.astype({'FracUso':'int'})
-PromedioHabiles=PromedioHabiles.sort_values(by ="NTransPromedio", ascending=False)
+PromedioHabiles=PromedioHabiles.sort_values(by ="NTransPromedio", ascending=False).reset_index()
 print(PromedioHabiles)
 
 ## realizo un gráfico
 fig, ax = plt.subplots(figsize=(8,4))
-ax.barh(PromedioHabiles['Ramal'].astype('str'), PromedioHabiles["NTrans"], color=["#12bc8e"])
+ax.barh(PromedioHabiles['Ramal'].astype('str'), PromedioHabiles["NumerodeTrans"], color=["#12bc8e"])
 
-for c in ax.containers:
-    labels = [(renglon for v in PromedioHabiles['FracUso'])]
-    ax.bar_label(c, labels=labels,label_type='edge',padding=0.3)
+rects = ax.patches
+# For each bar: Place a label
+for i, rect in enumerate(rects):
+    x_value = rect.get_width() / 2
+    y_value = rect.get_y() + rect.get_height() / 2
+    space = 5
+    ha = 'left'
+    # If value of bar is negative: Place label left of bar
+    if x_value < 0:
+        space *= -1
+    label = "{:.0f}%".format(PromedioHabiles.at[i,'FracUso'])
+    plt.annotate(label,                      # Use `label` as label
+        (x_value, y_value),         # Place label at end of the bar
+        xytext=(space, 0),          # Horizontally shift label by `space`
+        textcoords="offset points", # Interpret `xytext` as offset in points
+        va='center',                # Vertically center label
+        ha=ha, fontsize=12)
+
+
 ax.xaxis.set_tick_params(pad=5)
 ax.yaxis.set_tick_params(pad=5) # dist de las etiquetas
 ax.grid(color='green',linestyle='-.',linewidth=0.5,alpha=0.15)
 ax.invert_yaxis()
-ax.set_title(" Barplot de las transacciones promedio en un día hábil, por línea",
-             loc='center', fontname='Arial',fontsize='10', pad=5,color='black')
+ax.set_title("Barplot de las transacciones promedio en un día hábil, por línea.",
+             loc='center', fontname='Arial',fontsize='12', pad=5,color='black')
 plt.suptitle("Transacciones promedio por línea",
-              fontname='Arial',fontsize='20',color='black', fontweight='bold',y=0.99)
-plt.show()
+              fontname='Arial',fontsize='35',color='black', fontweight='bold',y=0.990)
+ax.text(0.5, 0.5, 'NonLinear', transform=ax.transAxes,
+            fontsize=80, color='gray', alpha=0.25,
+            ha='center', va='center', rotation=25)
+plt.annotate('Fuente: Datos Venado Tuerto, Octubre 2022', (0, 0), (0, -25), fontsize=9,
+                 xycoords='axes fraction', textcoords='offset points', va='top')
+
+
+
+#####
+
+#### MAPA DE CALOR
+pptPar= dfhabil.groupby(['Ramal','Hora']).size().to_frame('NumerodeTrans').reset_index()
+pptPar['NumerodeTrans'] = pptPar['NumerodeTrans'] / len(df['Dia'].unique())
+ax= sn.heatmap(pptPar.pivot(index="Ramal",columns="Hora",values="NumerodeTrans")
+,linewidth=1,vmin=(pptPar['NumerodeTrans']).min(),vmax=(pptPar['NumerodeTrans']).max()
+,cmap='Greens')
+ax.xaxis.set_tick_params(pad=5)
+ax.yaxis.set_tick_params(pad=5)
+ax.grid( color='green',
+            linestyle='-.', linewidth=0.5,
+            alpha=0.2)
+ax.set_title("Matriz de calor de transacciones por línea y hora", loc='center',fontsize=20,pad=10,color='darkblue')
+
+####
+
+
+
+#### Mapa de calor para un día hábil en particular
+lineas=df['Ramal'].dropna().unique()
+style1= style1 = {'fillColor' : '#5b5b5f', 'color': '#5b5b5f'}
+listadelista = []
+class HeatMapWithTimeAdditional(Layer):
+    _template = Template("""
+       {% macro script(this, kwargs) %}
+           var {{this.get_name()}} = new TDHeatmap({{ this.data }},
+               {heatmapOptions: {
+                   radius: {{this.radius}},
+                   minOpacity: {{this.min_opacity}},
+                   maxOpacity: {{this.max_opacity}},
+                   scaleRadius: {{this.scale_radius}},
+                   useLocalExtrema: {{this.use_local_extrema}},
+                   defaultWeight: 1,
+                   {% if this.gradient %}gradient: {{ this.gradient }}{% endif %}
+               }
+           }).addTo({{ this._parent.get_name() }});
+       {% endmacro %}
+   """)
+    def __init__(self, data, name=None, radius=15,
+                 min_opacity=0, max_opacity=0.6,
+                 scale_radius=False, gradient=None,
+                 use_local_extrema=False,overlay=True,
+                 control=True, show=True):
+        super(HeatMapWithTimeAdditional, self).__init__(
+            name=name, overlay=overlay, control=control, show=show
+        )
+        self._name = 'HeatMap'
+        self.data = data
+        # heatmap settings
+        self.radius = radius
+        self.min_opacity = min_opacity
+        self.max_opacity = max_opacity
+        self.scale_radius = 'true' if scale_radius else 'false'
+        self.use_local_extrema = 'true' if use_local_extrema else 'false'
+        self.gradient = gradient
+
+mapa = folium.Map([-33.74648, -61.9679], tiles="OpenStreetMap", zoom_start=14 )
+
+for indice in lineas:
+    auxiliar = df[df["Ramal"]==indice]
+    dfHeatTime= auxiliar.groupby(["latitude","longitude","Hora"]).size().to_frame('NumerodeTrans').reset_index().sort_values(by=["Hora"], ascending=True)
+    dfHeatTime['NumerodeTrans']=(dfHeatTime['NumerodeTrans']*10)/dfHeatTime['NumerodeTrans'].max()
+    dfHeatTime['Hora']= dfHeatTime['Hora'].sort_values(ascending=True)
+
+    for i in range(24): # incorporo un dato a cada hora para evitar error
+        dfAux= pd.DataFrame({'latitude':[-33.74648], 'longitude':[-61.9679], 'Hora':i, 'NumerodeTrans':[0.00001]})
+        dfHeatTime=pd.concat([dfHeatTime,dfAux])
+
+    data = [] #Aca genero una lista vacia, que sera la lista de listas, de una dimension = 24 (porque hay 24 hs)
+    for _, d in dfHeatTime.groupby('Hora'):
+       data.append([[row['latitude'], row['longitude'], row['NumerodeTrans']] for _, row in d.iterrows()])
+    listadelista += [data]
+
+for i in range(8):
+    a= gpd.read_file("Recorrido_TUP.shp")
+    fg = folium.FeatureGroup("LINEA"+" "+ str(lineas[i]))
+    if i==0:
+        plugins.HeatMapWithTime(data=listadelista[i], index=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
+        auto_play=True, display_index=True, name= str(lineas[i])).add_to(mapa)
+        folium.GeoJson(data=a["geometry"], style_function=lambda x:style1).add_to(fg)
+    else:
+        HeatMapWithTimeAdditional(data=listadelista[i], name= str(lineas[i])).add_to(fg),
+        folium.GeoJson(data=a["geometry"], style_function=lambda x: style1).add_to(fg)
+    fg.add_to(mapa)
+folium.LayerControl().add_to(mapa)
+loc = "Mapa de calor lineas 1 2 3 4 VT, Octubre 2022"
+title_html = '''
+                <h3 align="center" style="font-size:16px"><b>{}</b></h3>
+               '''.format(loc)
+mapa.get_root().html.add_child(folium.Element(title_html))
+mapa.save("Mapacalor"+".html")
+print ("Mapa Generado")
+#
--- VT-Preprocess.py
+++ VT-Preprocess.py
@@ -11,9 +11,8 @@ df= df[df["MES"]== 10]
 ## filtro los duplicados
 bool_series= df.duplicated(keep='first')
 df= df[~bool_series]
+print(df)
 
-# borro la primer columna
-df = df.drop(df.columns[0],axis=1)
 da = df
 
 ### saco el ! a las líneas
@@ -25,7 +24,13 @@ df['LINEA']=df.apply(lambda row: limp(row), axis=1)
 da = df
 # me llevo lo que no se repite
 df= df[["Interno","LINEA","Ramal","Tipo Trx","Fecha Hora","Hora","Tarifa Marcada","Tarifa Cobrada","Tarjeta","Legajo","Latitud","Longitud"]]
-df= df.rename(columns={"LINEA":"Linea"})
+df= df.rename(columns={"LINEA":"idlinea"})
+df= df.rename(columns={"Fecha Hora":"FECHATRX"})
+df= df.rename(columns={"Longitud":"longitude"})
+df= df.rename(columns={"Latitud":"latitude"})
+
+
+
+
 da.to_csv("VT_OCT.csv")
 df.to_csv("10-TX-SData.csv")
-
